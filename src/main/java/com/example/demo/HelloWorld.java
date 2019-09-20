cc3ebe53aa54233867149ebd866d155611bf3c07
Xiao-ShaBi <574601472@qq.com>
2019-09-20 16:37:58 +0800

    打包为不需要tomcat的服务

--- src/main/java/com/example/demo/HelloWorld.java
+++ src/main/java/com/example/demo/HelloWorld.java
@@ -1,19 +1,16 @@
 package com.example.demo;
 
-import com.example.service.ITest;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HelloWorld {
 
-    @Autowired
-    private ITest iTest;
+//    @Autowired
+//    private ITest iTest;
 
     @RequestMapping(value = "/")
     public String Hello() {
-        return iTest.test();
+        return "index";
     }
 }
